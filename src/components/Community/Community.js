a1f2e8a945b7a0a6d7e02f769690661d23e7774e
gurparshad <panksharma230@gmail.com>
2021-03-19 17:34:27 +0200

    createPost component added

--- src/components/Community/Community.js
+++ src/components/Community/Community.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Community = () => {
-  return <div>Community blogs</div>;
+  return (
+    <div>
+      <h3>You are not alone, Read stories and thoughts of other people</h3>
+      <button>Write Something...</button>
+    </div>
+  );
 };
 
 export default Community;
